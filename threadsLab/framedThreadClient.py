eaa5ad00065660795124a4bc98190106b0903b51
Fernando Martinez <fmartinez14@miners.utep.edu>
2018-10-13 14:39:03 -0600

    adding test folder as well as changing default parameters.

--- threadsLab/framedThreadClient.py
+++ threadsLab/framedThreadClient.py
@@ -9,7 +9,7 @@ import time
 
 
 switchesVarDefaults = ( #Default parameters
-    (('-s', '--server'), 'server', "127.0.0.1:50000"),
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
     (('-f', '--fileName') , 'file_name', "fileFromServer.txt"),
     (('-p', '--protocol') , 'protocol', "GET"),
     (('-d', '--debug'), "debug", False), # boolean (set if present)
